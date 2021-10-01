7817c1a28222c129533b762ba10528a1d25ddbd8
samueloh99 <samuel_oh@hotmail.com>
2021-09-30 22:06:23 -0300

    Added chakraui and configured enviroment

---
+++ src/style/chakraTheme.js
@@ -0,0 +1,31 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const chakraTheme = extendTheme({
+	colors: {
+		gray: {
+			900: "#181B23",
+			800: "#1F2029",
+			700: "#353646",
+			600: "#4B4D63",
+			500: "#616480",
+			400: "#797D9A",
+			300: "#9699B0",
+			200: "#B3B5C6",
+			100: "#D1D2DC",
+			50: "#EEEEF2",
+		},
+		black: {
+			900: "#252525",
+			700: "#3E3E3E",
+		},
+	},
+	styles: {
+		global: {
+			// styles for the `body`
+			body: {
+				bg: "black.900",
+				color: "white",
+			},
+		},
+	},
+});
