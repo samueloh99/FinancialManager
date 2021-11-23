b7d4c5db4eaf9fe6acfbe2aff1cb366ba8b61813
samueloh99 <samuel_oh@hotmail.com>
2021-11-22 22:30:16 -0300

    Changes

--- src/routes.js
+++ src/routes.js
@@ -14,24 +14,24 @@ import { AuthContext } from "./context/AuthContext";
 
 const Routes = () => {
 	const { isAuth } = useContext(AuthContext);
-	// if (isAuth) {
+	if (isAuth) {
+		return (
+			<BrowserRouter>
+				<Header />
+				<Flex h="100vh">
+					<Sidebar />
+					<Route component={Home} path="/" exact />
+					<Route component={Cliente} path="/clientes" />
+					<Route component={Financeiro} path="/financeiros" />
+				</Flex>
+			</BrowserRouter>
+		);
+	}
 	return (
 		<BrowserRouter>
-			<Header />
-			<Flex h="100vh">
-				<Sidebar />
-				<Route component={Home} path="/" exact />
-				<Route component={Cliente} path="/clientes" />
-				<Route component={Financeiro} path="/financeiros" />
-			</Flex>
+			<Route component={Login} exact path="/" />
 		</BrowserRouter>
 	);
-	// }
-	// return (
-	// 	<BrowserRouter>
-	// 		<Route component={Login} exact path="/" />
-	// 	</BrowserRouter>
-	// );
 };
 
 export default Routes;
